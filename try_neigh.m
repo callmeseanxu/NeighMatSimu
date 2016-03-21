09241bb2a213ceb5494ff99d59b9f0acc625a215
callmeseanxu <xuxiaoxiangtj@gmail.com>
2016-03-21 19:54:54 +0800

    now plot neigh match and neigh mutual

--- try_neigh.m
+++ try_neigh.m
@@ -1,17 +1,14 @@
 clear all;
 load('test1.mat');
 
-test_seconds = 600;
-picture_seconds = 10;
+test_seconds = 500;
+picture_seconds = 5;
 poor_radis = 40;
 best_radis = 10;
 neigh_max = 20;
 test_duration = test_seconds*100;   %this is 600s
 target_addr = 65;
 best_neigh_limit = 10;
-
-fid_neigh = fopen('neigh.txt','a');
-fid_best = fopen('bneigh.txt','a');
 
 neigh_present = zeros([test_seconds/picture_seconds, neigh_max]);
 
@@ -37,6 +34,8 @@ neigh_table(100,neigh_max).linkq = uint16(0);
 neigh_table_d(100,neigh_max).addr = 0;
 neigh_table_d(100,neigh_max).linkq = uint16(0);
 
+neigh_table_addr = zeros(100,neigh_max);
+
 for i = 1:100
     for j = 1:neigh_max
         neigh_table(i,j).addr = 0;
@@ -45,17 +44,8 @@ for i = 1:100
     end
 end
 
-j = 0;
-for i = 1:100
-    radis_ideal_neigh = pdist([coordinate_x(i),coordinate_y(i);coordinate_x(target_addr),coordinate_y(target_addr)]);
-    if (radis_ideal_neigh < poor_radis) && (i ~= target_addr)
-        j = j + 1;
-        ideal_neigh(j,1) = i;
-        ideal_neigh(j,2) = radis_ideal_neigh;
-    end
-end
-[Y, I] = sort(ideal_neigh(:,2), 'ascend');
-ideal_neigh_sort = ideal_neigh(I,:);
+neigh_match_percent = zeros(100,test_seconds/picture_seconds);
+mutual_match_percent = zeros(1,test_seconds/picture_seconds);
 
 for test_frame = 1:test_duration
     for node = 1:100
@@ -129,107 +119,70 @@ for test_frame = 1:test_duration
                 neigh_table_d(i,j).addr = neigh_table(i,j).addr;
             end
         end
-
-        j = 0;
-        for i = 1:neigh_max
-            if neigh_table_d(target_addr,i).addr > 0
-                j = j + 1;
-                neigh_target(j,1) = neigh_table_d(target_addr,i).addr;
-                neigh_target(j,2) = neigh_table_d(target_addr,i).linkq;
-            end
-        end
-
-        group = zeros([1 100]);
-        [Y, I] = sort(neigh_target(:,2), 'descend');
-        neigh_target_sort = neigh_target(I,:);
-
-
-        %group 0 is all
-        %group 1 is within range
-        %group 2 is in neigh table
-        %group 3 is best neigh
-        %group 4 is self
-        for i = 1:length(neigh_target)
-            group(neigh_target(i,1)) = 2;
-        end
-
-        if length(neigh_target) > best_neigh_limit
-            for i = 1:best_neigh_limit
-                group(neigh_target_sort(i,1)) = 3;
-            end
-        end
-
-        for i = 1:100
-            if(pdist([coordinate_x(target_addr),coordinate_y(target_addr);coordinate_x(i),coordinate_y(i)]) < poor_radis)
-                if group(i) == 0
-                    group(i) = 1;
-                end
-            end
-        end
-
-        best_neigh_match_count = 0;
-        for i = 1:best_neigh_limit
-            for j = 1:best_neigh_limit
-                if neigh_target_sort(i,1) == ideal_neigh_sort(j,1)
-                    best_neigh_match_count = best_neigh_match_count + 1;
-                end
-            end
-        end
-        fprintf(fid_best, '%d\n', best_neigh_match_count);
-
-        neigh_match_count = 0;
-        if length(neigh_target_sort) == neigh_max
-            for i = 1:neigh_max
-                for j = 1:neigh_max
-                    if neigh_target_sort(i,1) == ideal_neigh_sort(j,1)
-                        neigh_match_count = neigh_match_count + 1;
-                    end
-                end
-            end
-        else
-            for i = 1:length(neigh_target_sort)
-                for j = 1:length(neigh_target_sort)
-                    if neigh_target_sort(i,1) == ideal_neigh_sort(j,1)
-                        neigh_match_count = neigh_match_count + 1;
-                    end
-                end
-            end
-        end
-        fprintf(fid_neigh, '%d\n', neigh_match_count); 
-
-        group(target_addr) = 4;
-        gscatter(coordinate_x, coordinate_y, group,'kmgrb','xosdp');
-        xlabel([' ' num2str(neigh_match_count) ' neigh match,  ' num2str(best_neigh_match_count) ' best match,  ' num2str(length(ideal_neigh)) ' in range']);
-        title(num2str(test_frame));
-
-        filename = sprintf(num2str(test_frame));
-        filename = regexprep(filename,':','-','all');
-        print('-dpng','-zbuffer','-r200',filename);
-        disp(['frame ' num2str(test_frame) ' finished']);
         
-        for i = 1:neigh_max
-            neigh_present(uint8(test_frame/(picture_seconds*100)),i) = neigh_table_d(target_addr,i).addr;
-        end
+        for target_addr = 1:100
+            j = 0;
+            for i = 1:neigh_max
+                if neigh_table_d(target_addr,i).addr > 0
+                    j = j + 1;
+                    neigh_target(j,1) = neigh_table_d(target_addr,i).addr;
+                    neigh_target(j,2) = neigh_table_d(target_addr,i).linkq;
+                end
+            end
 
+            [~, I] = sort(neigh_target(:,2), 'descend');
+            neigh_target_sort = neigh_target(I,:);
+
+            j = 0;
+            for i = 1:100
+                radis_ideal_neigh = pdist([coordinate_x(i),coordinate_y(i);coordinate_x(target_addr),coordinate_y(target_addr)]);
+                if (radis_ideal_neigh < poor_radis) && (i ~= target_addr)
+                    j = j + 1;
+                    ideal_neigh(j,1) = i;
+                    ideal_neigh(j,2) = radis_ideal_neigh;
+                end
+            end
+            [~, I] = sort(ideal_neigh(:,2), 'ascend');
+            ideal_neigh_sort = ideal_neigh(I,:);
+
+            best_neigh_match_count = 0;     
+            neigh_max_compare = min(min(length(neigh_target_sort(:,1)),best_neigh_limit),min(length(ideal_neigh_sort(:,1)),best_neigh_limit));
+            for i = 1:neigh_max_compare
+                for j = 1:neigh_max_compare
+                    if neigh_target_sort(i,1) == ideal_neigh_sort(j,1)
+                        best_neigh_match_count = best_neigh_match_count + 1;
+                    end
+                end
+            end
+
+            neigh_match_percent(target_addr, test_frame/picture_seconds/100) = best_neigh_match_count/neigh_max_compare;
+            for i = 1:100
+                for j = 1:neigh_max
+                    neigh_table_addr(i,j) = neigh_table_d(i,j).addr;
+                end
+            end
+            
+            total_edges = 0;
+            double_edges = 0;
+            for i = 1:100
+                for j = 1:neigh_max
+                    if(neigh_table_addr(i,j) > 0)
+                        total_edges = total_edges + 1;
+                        if isempty(find(neigh_table_addr(neigh_table_addr(i,j),:) == i)) == 0
+                            double_edges = double_edges + 1;
+                        end
+                    end
+                end
+            end
+            mutual_match_percent(test_frame/picture_seconds/100) = double_edges/total_edges;
+        end
     end
 end
 
-fclose(fid_neigh);
-fclose(fid_best);
+plot(mean(neigh_match_percent));
+xlabel([' average match neigh percent,  ' num2str(mean(mean(neigh_match_percent))) ' ']);
+print('-dpng','-zbuffer','-r200',' match_neigh');
 
-for i = 1:best_neigh_limit
-    neigh_present(test_seconds/picture_seconds + 1,i) = ideal_neigh_sort(i,1);
-end
-
-for i = 1:neigh_max
-    neigh_present(test_seconds/picture_seconds + 2,i) = ideal_neigh_sort(i,1);
-end
-
-x = 1:test_seconds/picture_seconds + 2;
-for i = 1:neigh_max
-    scatter(x,neigh_present(:,i),'.','LineWidth', 1.2)
-    hold on
-end
-
-print('-dpng','-zbuffer','-r200','neigh_view');
-
+plot(mutual_match_percent);
+xlabel([' average mutual neigh percent,  ' num2str(mean(mutual_match_percent)) ' ']);
+print('-dpng','-zbuffer','-r200','mutual_neigh');
